eb714059de94ca34581870fb46f77d8ada358fd3
tech-horizon01 <techhorizonschoolofficial@gmail.com>
2025-05-04 00:24:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "947437XXXXX",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94726740966",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "TECH HORIZON SCHOOL INC",
 // add bot owner name
@@ -73,7 +73,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "94743706283",
+DEV: process.env.DEV || "94726740966",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
